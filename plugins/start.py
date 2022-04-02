d85b2b7323df8dc04287aaba37bb57c9ee0b3fb5
legion <88172317+LegacyOO7@users.noreply.github.com>
2022-04-02 22:10:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -114,7 +114,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "▶Join Channel",
                 url = client.invitelink)
         ]
     ]
@@ -122,7 +122,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = '♻Try Again',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
